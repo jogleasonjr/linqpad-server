eb551bc943873de377847e9c797b64756bc3fdc0
John Gleason <jogleasonjr@gmail.com>
2016-04-24 12:18:50 -0500

    .gitignore is now working

--- LinqpadServer.Tests/TestHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace LinqpadServer.Tests
-{
-    public static class TestHelper
-    {
-        public static DirectoryInfo TestSnippetDir { get; } = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "snippets"));
-    }
-}
